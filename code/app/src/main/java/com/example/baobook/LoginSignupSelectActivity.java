6c1cf54b41dde63a877d2e7944de678c04a3454a
Chloe Haynes <chloehaynes5252@gmail.com>
2025-03-03 14:01:56 -0700

    profile stuff

--- code/app/src/main/java/com/example/baobook/LoginSignupSelectActivity.java
+++ code/app/src/main/java/com/example/baobook/LoginSignupSelectActivity.java
@@ -2,6 +2,7 @@ package com.example.baobook;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,19 +16,28 @@ public class LoginSignupSelectActivity extends AppCompatActivity {
         setContentView(R.layout.login_signup_select_activity);
         Button loginButton = findViewById(R.id.LoginButton);
         Button signupButton = findViewById(R.id.SignUpButton);
-        loginButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
+        loginButton.setOnClickListener(v-> {
                 Intent intent = new Intent(LoginSignupSelectActivity.this, LoginActivity.class);
                 startActivity(intent);
-            }
         });
-        signupButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
+        signupButton.setOnClickListener(v ->{
                 Intent intent = new Intent(LoginSignupSelectActivity.this, SignupActivity.class);
                 startActivity(intent);
-            }
         });
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        SharedPreferences preferences = getSharedPreferences("UserSession", MODE_PRIVATE);
+        String userId = preferences.getString("userId", null);
+
+        if (userId != null) {
+            // User is already logged in, go to main app screen
+            Intent intent = new Intent(this, Home.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
 }
